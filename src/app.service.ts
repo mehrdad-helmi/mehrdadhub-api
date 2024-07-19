ecabf6ded1767e5ab7fcb9b18a458ba5b85e7c33
Mehrdad Helmi <mehrdad.redhat@gmail.com>
2024-07-20 02:22:50 +0330

    ci: Final production deployment action added  - Hotfix action and scripts added

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { DateTime } from 'luxon';
 
 @Injectable()
 export class AppService {
@@ -8,7 +9,8 @@ export class AppService {
     return {
       greeting:'Welcome to the Mehrdadhub API 😊',
       apiVersion:this.configService.get('minorVersion'),
-      environment:this.configService.get('NODE_ENV')
+      environment:this.configService.get('NODE_ENV'),
+      serverTime:DateTime.now().toLocaleString(DateTime.DATETIME_HUGE_WITH_SECONDS)
     };
   }
 }
